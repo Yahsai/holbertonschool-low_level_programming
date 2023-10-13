9d8d0426ef610cedd3e6dd4fdd9a74eb976754f5
Yahsai <yahsaisantana1@gmail.com>
2023-10-13 14:32:12 -0400

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - prints a string using puts 
+ *
+ * Return: 0 (Succes)
+ */
+
+int main(void)
+{
+puts("\Programing is  like building a multilingual puzzle");
+return (0);
+}
