165e22ac1632b0ddd1243607bca4bb57c9e98141
Yahsai <yahsaisantana1@gmail.com>
2023-10-18 11:32:07 -0400

    4-print_alphabt

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ *main-Entry point
+ *
+ *Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'q' && i != 'e')
+		{
+			putchar(i);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
