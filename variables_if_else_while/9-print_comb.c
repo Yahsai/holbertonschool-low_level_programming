37f3a12f9016f48d4e10831f48c63d867dee131e
Yahsai <yahsaisantana1@gmail.com>
2023-10-19 10:02:33 -0400

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -6,18 +6,18 @@
  */
 int main(void)
 {
-
 	int i;
 
 
 	for (i = '0'; i <= '9'; i++)
 	{
+		putcar(i + '0');
+
 		if (i < 9)
 		{
-		putchar(i + '0');
+			putchar(',');
+			putchar(' ');
 		}
-	putchar(',');
-    putchar(' ');
 	}
 	putchar('\n');
 	return (0);
