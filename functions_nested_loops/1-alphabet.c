8e7eaa40fbc20af36dd56ffeaceabe8b3000d456
Yahsai <yahsaisantana1@gmail.com>
2023-10-19 13:37:39 -0400

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,16 +1,17 @@
-#include"main.h"
+#include <stdio.h>
+#include "main.h"
+
 /**
- *main- entry point
- *Return: always 0
+ * *main - entry point
+ * Return: always 0 (success)
  */
-void print_alphabet(void);
-
-char c;
-
-for (c = 'a'; c <= 'z'; i++)
+void print_alphabet(void)
 {
-	_putchar (i);
-}
-_putchar ('\n');
-return (0);
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar ((int)c);
+	}
+	putchar ('\n');
 }
