77a5b7c713b7aaa86e554ba140b9c226825fe478
Yahsai <yahsaisantana1@gmail.com>
2023-10-19 14:27:37 -0400

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,17 +7,17 @@
  *
  *Return: void
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	char c;
 	int i;
-}
+
 	for (c = 'a'; c <= 'z'; c++)
-
-	for (i = '1'; i <= '10'; i++)
-{
-	putchar ((int)c);
+	{
+		for (i = 1; i <= 10; i++)
+		{
+			putchar ((int)c);
+		}
+	}
+	putchar ('\n');
 }
-putchar ('\n')
-return (0);
-
