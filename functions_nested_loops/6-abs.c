0093f8d1dbe2f89721c8db6205538b8bee3610c6
Yahsai <yahsaisantana1@gmail.com>
2023-10-20 14:22:28 -0400

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ *_abs - function that calculates the absolute value of an integer
+ *@n: integer to check
+ *Return: n
+ */
+int _abs(int);
+{
+	if (n>= 0)
+		return (n);
+
+	else
+	{
+		n *= 1;
+		return (n);
+	}
+}
+
