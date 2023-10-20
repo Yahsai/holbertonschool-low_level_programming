2f6ff5bb38c79ca432c89017d6f9d82ed9a13ce0
Yahsai <yahsaisantana1@gmail.com>
2023-10-20 10:58:31 -0400

    4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * isalpha -returns 1 if c is lowecase and returns 0 otherwise
+ * @c: is the imput character
+ * Return: 2.
+ */
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+			{
+		return (1);
+		}
+
+	else
+	{
+		if (c >= 'a' && c <= 'z')
+		return (0);
+}
+
