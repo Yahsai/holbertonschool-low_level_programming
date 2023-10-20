ebaa15456860c5e8775ef9459aa6b8d08f651568
Yahsai <yahsaisantana1@gmail.com>
2023-10-20 13:29:03 -0400

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,25 +1,25 @@
 #include"main.h"
 /**
- *print_sign - checks integar sign
- *@n: integer to check
+ *print_sign- checks integar sign
+ *@n:integar to check
  *
- * Return: 0 or 1
+ * Return:0 or 1
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar (43);
+		putchar(43);
 		return(1);
 	}
 	else if (n == 0)
 	{
-		putchar (48);
+		putchar(48);
 		return(0);
 	}
 	else
 	{
-		putchar (45);
+		putchar(45);
 		return(-1);
 	}
 }
