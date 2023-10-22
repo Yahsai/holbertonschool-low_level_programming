c63f4ac5748d2b82c46de99db69caef5686ccf20
Yahsai <yahsaisantana1@gmail.com>
2023-10-21 22:06:07 -0400

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+ * add - adds two numbers together
+ * @i: the first number to be added
+ * @e: the second number to be added
+ * Return: the sum of two numbers
+ */
+int add(int i, int e)
+{
+	return (i + e);
+}
