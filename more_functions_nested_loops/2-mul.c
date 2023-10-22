e363104a5d75676d06870e0efe5258cf30d50ca5
Yahsai <yahsaisantana1@gmail.com>
2023-10-22 15:15:36 -0400

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *mul - multiplies integers
+ *@a: first digit
+ *@b: second digit
+ *Return: integer
+ */
+
+int mul(int a, int b)
+{
+
+	int c = a * b;
+
+	return (c);
+
+}
