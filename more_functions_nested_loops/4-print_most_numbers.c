597d09ca828ce1bbfdea51f891ff42bf4be3cd0b
Yahsai <yahsaisantana1@gmail.com>
2023-10-22 15:19:25 -0400

    files adjusted

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_most_numbers - print number
+ */
+
+void print_most_numbers(void)
+{
+
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+		{
+		putchar(i);
+		}
+	}
+
+	putchar('\n');
+
+}
