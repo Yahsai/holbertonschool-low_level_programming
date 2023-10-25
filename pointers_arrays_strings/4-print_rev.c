8053acaf2ff627747828290e9b424c14450d1ad9
Yahsai <yahsaisantana1@gmail.com>
2023-10-25 12:54:21 -0400

    4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints reversed string
+ * @s: receives pointer
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int j;
+	int a = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		a++;
+	}
+
+	for (j = (a - 1); j >= 0; j--)
+	{
+	_pucthar(s[j]);
+	}
+	_putchar('\n');
+}
