5990c1e8c1e1dc0315beda35e06f289501f026ac
Yahsai <yahsaisantana1@gmail.com>
2023-10-25 16:55:26 -0400

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - reverses string
+ *@s: receives char
+ */
+
+void rev_string(char *s)
+{
+	char temp;
+	int i;
+	int len;
+
+	for (len = 0; s[len] != '\0';)
+		len++;
+
+	for (i = len - 1; i >= len / 2; i--)
+	{
+
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
