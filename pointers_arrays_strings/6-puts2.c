9d2792a7b1ae97492a7632d531e1076e981f9f6f
Yahsai <yahsaisantana1@gmail.com>
2023-10-26 10:01:25 -0400

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - puts 2
+ * @str: receives pointer
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			putchar(str[i]);
+		}
+	}
+
+	putchar('/n');
+}
