ac1f67d0db2e86829893ffe05ce93d11ea758756
Yahsai <yahsaisantana1@gmail.com>
2023-10-26 11:06:35 -0400

    8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+ * print_array - prints array
+ * @a: receives pointer
+ * @n: receives int
+ */
+
+void print_array(int *a, int n)
+{
+
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+		printf("\n");
+}
