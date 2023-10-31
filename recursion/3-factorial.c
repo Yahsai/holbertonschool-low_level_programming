d691fb0969fa7881f53bfc1c13086dfff51e27ad
Yahsai <yahsaisantana1@gmail.com>
2023-10-31 14:01:13 -0400

    3-factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * factorial - factial of a number
+ * @n: receives int
+ * Return: integer
+ */
+
+int factorial(int n)
+{
+
+	if (n >= 1)
+	{
+		return (n * factorial(n - 1));
+	}
+
+	else if (n == 0)
+	{
+	return (1);
+	}
+
+	else
+	{
+		return (-1);
+	}
+}
