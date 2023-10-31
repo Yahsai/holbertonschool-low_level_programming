44fc9ed676856c8736701070db2337c85186cfe2
Yahsai <yahsaisantana1@gmail.com>
2023-10-31 14:27:42 -0400

    4-pow_recursion.c

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - raised to the power
+ * @x: value
+ * @y:power
+ * Return: integer
+ */
+
+int _pow_recursion(int x, int y)
+{
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	else if (y == 0)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+
+}
