91f8c6c17e75972acda5fa81ac7e1830cc6a931e
Yahsai <yahsaisantana1@gmail.com>
2023-11-02 15:23:47 -0400

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *@argc: argument counter
+ *@argv: argument vector
+ *Return: integer
+ */
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+
+}
