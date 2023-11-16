4c467d0b13149e88db9c8695944038021a39cb82
Yahsai <yahsaisantana1@gmail.com>
2023-11-16 14:04:49 -0400

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,23 +2,23 @@
 #include <stddef.h>
 
 /**
- * init_dog - initializes variable of type
- * @d: struct dog
- * @name: char name
- * @age: float age
- * @owner: char owner
+ *init_dog - initializes variable of type
+ *@d: struct dog
+ *@name: char name
+ *@age: float age
+ *@owner: char owner
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-  if (d == NULL)
-  {
-  }
+	if (d == NULL)
+	{
+	}
 
-  else
-  {
-  d->name = name;
-  d->age = age;
-  d->owner = owner;
-  }
+	else
+	{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	}
 }
