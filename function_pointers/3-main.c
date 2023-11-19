64558c75b3ba31ff0ccbb42f7e6c758d356bb075
Yahsai <yahsaisantana1@gmail.com>
2023-11-19 01:43:44 -0400

    3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,44 +1,38 @@
+#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "3-calc.h"
-
-/**
- * get_op_func - gets a pointer to the function corresponding to the given operator
- * @s: string representing the operator
- * Return: pointer to the function corresponding to the operator or NULL if not found
- */
-int (*get_op_func(char *s))(int, int);
 
 /**
  * main - main function
  * @argc: number of command-line arguments
  * @argv: array of command-line argument strings
- * Return: 0 if executed successfully, 98 for incorrect number of arguments, 99 for invalid operator
+ * Return: 0 if executed successfully, 98 for incorrect number of arguments,
+ *         99 for invalid operator, 100 for division by zero error
  */
 int main(int argc, char *argv[]) {
+    int num1, num2, result;
+    int (*op)(int, int);
+
     if (argc != 4) {
         printf("Error\n");
-        return 98;
+        return 98;  /* Error code 98 for incorrect number of arguments */
     }
 
-    int num1 = atoi(argv[1]);
-    int num2 = atoi(argv[3]);
-    char *operator = argv[2];
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
 
-    if (*operator != '+' && *operator != '-' && *operator != '*' && *operator != '/' && *operator != '%') {
-        printf("Error\n");
-        return 99;
-    }
-
-    int (*op)(int, int) = get_op_func(operator);
+    op = get_op_func(argv[2]);
 
     if (op == NULL) {
         printf("Error\n");
-        return 99;
+        return 99;  /* Error code 99 for invalid operator */
     }
 
-    printf("%d\n", op(num1, num2));
-    return 0;  
+    result = op(num1, num2);
+
+    printf("%d\n", result);
+
+    return 0;  /* Success */
 }
 
