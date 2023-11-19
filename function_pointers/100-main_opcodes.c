7c4763699ba1be0dc54526094b3d08fff969bb32
Yahsai <yahsaisantana1@gmail.com>
2023-11-19 01:49:42 -0400

    100-main_opcodes.c

---
+++ function_pointers/100-main_opcodes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of command-line arguments
+ * @argv: array of command-line argument strings
+ * Return: 0 if executed successfully, 1 for incorrect number of arguments,
+ *         2 for negative number of bytes
+ */
+int main(int argc, char *argv[]) {
+    int num_bytes, i;
+
+    if (argc != 2) {
+        printf("Error\n");
+        return 1;  /* Error code 1 for incorrect number of arguments */
+    }
+
+    num_bytes = atoi(argv[1]);
+
+    if (num_bytes < 0) {
+        printf("Error\n");
+        return 2;  /* Error code 2 for negative number of bytes */
+    }
+
+    /* Print opcodes */
+    for (i = 0; i < num_bytes && i < (int)main; i++) {
+        printf("%02x ", *((unsigned char*)main + i));
+    }
+
+    printf("\n");
+
+    return 0;  /* Success */
+}
+
