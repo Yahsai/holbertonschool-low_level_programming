d45825b66cc1cb8d5b367ba57296a83c0adb15d7
Yahsai <yahsaisantana1@gmail.com>
2023-11-19 01:36:15 -0400

    3-op_functions.c

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,77 +1,63 @@
+#include <stdio.h>
 #include "3-calc.h"
 #include <stdlib.h>
-#include <stdio.h>
+
 
 /**
- * op_add - adds two integers
+ * op_add - returns the sum of a and b
  * @a: first integer
  * @b: second integer
  * Return: sum of a and b
  */
-int op_add(int a, int b) { return a + b; }
+int op_add(int a, int b) {
+    return a + b;
+}
 
 /**
- * op_sub - subtracts two integers
+ * op_sub - returns the difference of a and b
  * @a: first integer
  * @b: second integer
  * Return: difference of a and b
  */
-int op_sub(int a, int b) { return a - b; }
+int op_sub(int a, int b) {
+    return a - b;
+}
 
 /**
- * op_mul - multiplies two integers
+ * op_mul - returns the product of a and b
  * @a: first integer
  * @b: second integer
  * Return: product of a and b
  */
-int op_mul(int a, int b) { return a * b; }
-
-/**
- * op_div - divides two integers
- * @a: dividend
- * @b: divisor
- * Return: quotient of a and b, or error message if b is 0
- */
-int op_div(int a, int b) { return b ? a / b : (printf("Error: Division by zero\n"), 0); }
-
-/**
- * op_mod - calculates the modulus of two integers
- * @a: dividend
- * @b: divisor
- * Return: remainder of a divided by b, or error message if b is 0
- */
-int op_mod(int a, int b) { return b ? a % b : (printf("Error: Modulo by zero\n"), 0); }
-
-/**
- * get_op_func - gets a pointer to the function corresponding to the given operator
- * @s: string representing the operator
- * Return: pointer to the function corresponding to the operator or NULL if not found
- */
-int (*get_op_func(char *s))(int, int) {
-    int i;
-    int (*ops[])(int, int) = {op_add, op_sub, op_mul, op_div, op_mod};
-
-    for (i = 0; i < 5; ++i)
-        if (*s == *("+*/-" + i))
-            return ops[i];
-
-    return NULL;
+int op_mul(int a, int b) {
+    return a * b;
 }
 
 /**
- * main - main function
- * Return: 0 if executed successfully
+ * op_div - returns the result of the division of a by b
+ * @a: dividend
+ * @b: divisor
+ * Return: result of the division of a by b
  */
-int main() {
-    char operator = '+';
-    int a = 10, b = 5;
-    int (*op)(int, int) = get_op_func(&operator);
+int op_div(int a, int b) {
+    if (b == 0) {
+        printf("Error\n");
+        exit(100);
+    }
+    return a / b;
+}
 
-    if (op)
-        printf("%d %c %d = %d\n", a, operator, b, op(a, b));
-    else
-        printf("Invalid operator\n");
-
-    return 0;
+/**
+ * op_mod - returns the remainder of the division of a by b
+ * @a: dividend
+ * @b: divisor
+ * Return: remainder of the division of a by b
+ */
+int op_mod(int a, int b) {
+    if (b == 0) {
+        printf("Error\n");
+        exit(100);
+    }
+    return a % b;
 }
 
