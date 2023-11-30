09ab4e13be3f58b72a018b6b8cc475fea7d1bc28
Yahsai <yahsaisantana1@gmail.com>
2023-11-30 16:15:14 -0400

    advanced

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,20 +1,21 @@
+#include <stdio.h>
 #include "lists.h"
-#include<stdio.h>
+
+void before_main(void);
 
 /**
- * main - check the code
- * Return: always 0
+ * main - Entry point of the program
+ *
+ * Return: Always 0.
  */
+int main(void) {
+    before_main();
 
-void before_main() __attribute__((constructor));
-void after_main() __attribute__((destructor));
-void before_main()
-{
-   printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+    return (0);
 }
-void after_main()
-{
-   printf("A tortoise, having pretty good sense of a hare's nature, challenges one to a race,\n"
 
-		   return (0);
+void before_main(void) {
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
+
