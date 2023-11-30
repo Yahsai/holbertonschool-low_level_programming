be8e783fe78b085abf0be833f9e64d641f658949
Yahsai <yahsaisantana1@gmail.com>
2023-11-30 16:10:51 -0400

    advanced

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,11 +1,20 @@
 #include "lists.h"
+#include<stdio.h>
 
 /**
- * yahsai - prints text before main function executes
+ * main - check the code
+ * Return: always 0
  */
 
-void __attribute__ ((constructor)) yahsai(void)
+void before_main() __attribute__((constructor));
+void after_main() __attribute__((destructor));
+void before_main()
 {
-	printf("You're beat! and yet, you must allow,\n"
-	       "I bore my house upon my back!\n");
+   printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+void after_main()
+{
+   printf("A tortoise, having pretty good sense of a hare's nature, challenges one to a race,\n"
+
+		   return (0);
 }
