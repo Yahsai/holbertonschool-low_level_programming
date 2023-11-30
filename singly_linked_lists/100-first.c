f23681b92792de84aabd794f22d9fa4ae70e54a7
Yahsai <yahsaisantana1@gmail.com>
2023-11-30 00:55:24 -0400

    new advanced

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+#include "lists.h"
 
-void before_main(void)
+void before_main(void);
+
+/**
+ * main - main function
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-
+    before_main();
 
     return (0);
+}
+
+void before_main(void) {
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
 
