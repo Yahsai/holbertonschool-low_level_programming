96b2a60f51a3e056e0b26a2925fb107af36b5cb9
Yahsai <yahsaisantana1@gmail.com>
2023-11-30 15:41:53 -0400

    function that adds a new node at the beginning of a list_t list

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,21 +10,22 @@
  *
  * Return: the address of the new element, or NULL if it failed
  */
-list_t *add_node(list_t **head, const char *str) {
+list_t *add_node(list_t **head, const char *str)
+{
     list_t *new_node;
 
     if (head == NULL || str == NULL) {
-        return NULL;  // Check for NULL parameters
+        return NULL;
     }
 
     new_node = malloc(sizeof(list_t));
     if (new_node == NULL) {
-        return NULL;  // Memory allocation failed
+        return NULL;
     }
 
     new_node->str = strdup(str);
     if (new_node->str == NULL) {
-        free(new_node);  // String duplication failed, free the allocated memory
+        free(new_node);
         return NULL;
     }
 
