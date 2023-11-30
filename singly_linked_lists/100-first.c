7250f8f88e3caa3b73a61bf314e989e699ff5a77
Yahsai <yahsaisantana1@gmail.com>
2023-11-30 16:19:39 -0400

    advanced

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,21 +1,10 @@
 #include <stdio.h>
-#include "lists.h"
-
-void before_main(void);
 
 /**
- * main - Entry point of the program
- *
- * Return: Always 0.
+ * before_main - prints specified lines before main function
  */
-int main(void) {
-    before_main();
-
-    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-    return (0);
-}
+void before_main(void) __attribute__((constructor));
 
 void before_main(void) {
     printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
-
